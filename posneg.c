ef1c81f3701566df1a0ae7a5cbbaedaa7dc41452
deepu2196 <pradeep2196@gmail.com>
2017-02-24 14:30:02 +0530

    Create posneg.c

---
+++ posneg.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n;
+scanf("%d",&n);
+if(n!=0)  
+{
+  if(n>0)
+    printf("positive");
+    else
+    printf("negative");
+  }
+ else
+ printf("zero");
+ return 0;
+ }
